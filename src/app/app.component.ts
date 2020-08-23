396404e26932a367ee4b37b76493fa2c7828df78
Alessandra Adami <adami.alessandra88@gmail.com>
2020-08-23 07:42:40 +0200

    412. Grouping Transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { 
+  trigger, 
+  state, 
+  style, 
+  transition, 
+  animate, 
+  keyframes,
+  group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -59,6 +66,47 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
           opacity: 0
         }))
       ])
+    ]),
+    trigger('list2',[
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void <=> *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* <=> void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])        
+      ])
     ])
   ]
 })
@@ -79,4 +127,8 @@ export class AppComponent {
     onAdd(item) {
       this.list.push(item);
     }
+
+  onDelete(index: number) {
+    this.list.splice(index, 1);
+  }
 }
